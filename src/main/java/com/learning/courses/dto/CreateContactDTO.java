3489e55b1adcd92adbcc26f13af62cebe4faa22e
JakubChalimoniuk <jakub.chalimoniuk33@gmail.com>
2026-05-20 23:02:37 +0200

    Refactor contact table schema and update validation in CreateContactDTO

--- src/main/java/com/learning/courses/dto/CreateContactDTO.java
+++ src/main/java/com/learning/courses/dto/CreateContactDTO.java
@@ -2,8 +2,10 @@ package com.learning.courses.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,18 +21,17 @@ import java.io.Serializable;
 public class CreateContactDTO implements Serializable {
 
     @NotBlank
-    @Schema(example = "Domowy")
+    @Schema(example = "Home / Work")
     private String name;
 
-    @NotBlank
+    @Email(message = "Invalid email format")
     @Schema(example = "student@example.com")
     private String email;
 
-    @NotBlank
     @Schema(example = "ul. Akademicka 1, 00-000 Warszawa")
     private String address;
 
-    @NotBlank
+    @Pattern(regexp = "^\\+?[0-9\\-\\s]*$", message = "Invalid phone number format")
     @Schema(example = "123-456-789")
     private String phoneNumber;
 
--- src/test/java/com/learning/courses/ContactIT.java
+++ src/test/java/com/learning/courses/ContactIT.java
@@ -118,4 +118,22 @@ public class ContactIT extends AbstractIntegrationTest {
             assertThat(contactRepository.existsById(EXISTING_CONTACT_ID)).isFalse();
         }
     }
+    @Test
+    @Sql("/sql/CreateContactInit.sql")
+    void shouldDeleteContactsWhenPersonIsDeleted() throws Exception {
+        // given
+        final HttpDelete httpDelete = new HttpDelete("/api/persons/" + STUDENT_ID);
+
+        assertThat(contactRepository.existsById(EXISTING_CONTACT_ID)).isTrue();
+
+        // when
+        try (var client = HttpClients.createDefault()) {
+            var response = client.execute(getHttpHost(), httpDelete);
+
+            // then
+            assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.OK.value());
+
+            assertThat(contactRepository.existsById(EXISTING_CONTACT_ID)).isFalse();
+        }
+    }
 }
